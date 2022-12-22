e082339a458e0440f49f229e758e964b157f1a75
MalkahSimrahKhan <120410760+MalkahSimrahKhan@users.noreply.github.com>
2022-12-22 18:32:12 +0000

    Add files via upload
    //Addition of two numbers.
    1.Start.
    2.Read x and y.
    3.Sum=x+y.
    4.Write sum.
    5.End.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{//Add two numbers
+    int x,y,sum;
+    cout<< "Enter 1st number" << endl;
+    cin>>x;
+    cout<< "Enter 2nd number"<<endl;
+    cin>>y;
+    sum=x+y;
+    cout<<"sum="<<sum<<endl;
+    return 0;
+}
